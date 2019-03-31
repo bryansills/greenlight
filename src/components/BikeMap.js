d88cf053c39d044daaae1d43705d55557288614f
Bryan Sills <bryan@bryansills.com>
2019-03-31 09:40:35 -0500

    Display Leaflet map

---
+++ src/components/BikeMap.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Map, TileLayer, Marker, Popup } from "react-leaflet"
+
+export class BikeMap extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 13
+        }
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng]
+
+        if (typeof window !== 'undefined') {
+            return (
+                <Map center={position} zoom={this.state.zoom} style={{height: 800}}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </Map>
+            )
+        } else {
+            return null
+        }
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Header } from "../components/Header"
+import { BikeMap } from "../components/BikeMap"
 
 export default () => {
     return (
         <div style={{ color: `teal` }}>
             <Header />
             <p>Such wow. Very React.</p>
+            <BikeMap />
         </div>
     )
 }
